5cfd79dc1ee45556a1ff9c58ab2cb6f58a718234
Michaela <michaela.philip@emory.edu>
2024-03-05 15:39:28 -0500

    no row limit

--- import_data.py
+++ import_data.py
@@ -4,16 +4,16 @@ from helpers.read_data import read_data
 from helpers.merge_data import merge_data
 print('function import complete')
 
-household_plan_year = read_data('./data/input/household_plan_year777.csv', limit = 100)
+household_plan_year = read_data('./data/input/household_plan_year777.csv')
 print('household_plan_year import complete')
 print(household_plan_year.info())
 
-households = read_data('./data/input/households777.csv', limit = 100)
+households = read_data('./data/input/households777.csv')
 print('households import complete')
 households.rename(columns = {'choice': 'plan_name'}, inplace = True)
 print(households.info())
 
-plans = read_data('./data/input/plans777.csv', limit = 100)
+plans = read_data('./data/input/plans777.csv')
 print('plans import complete')
 print(plans.info())
 
