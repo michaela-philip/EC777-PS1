04aeebfe8f67e23426158f89a78ac417f89fe8d1
Michaela <michaela.philip@emory.edu>
2024-03-06 13:38:49 -0500

    remove limit

--- data_code/import_data.py
+++ data_code/import_data.py
@@ -9,16 +9,16 @@ from helpers.read_data import read_data
 from helpers.merge_household_plan import merge_data
 print('function import complete')
 
-household_plan_year = read_data('./data/input/household_plan_year777.csv', limit=1000)
+household_plan_year = read_data('./data/input/household_plan_year777.csv')
 print('household_plan_year import complete')
 print(household_plan_year.info())
 
-households = read_data('./data/input/households777.csv', limit=1000)
+households = read_data('./data/input/households777.csv')
 print('households import complete')
 households.rename(columns = {'choice': 'plan_name'}, inplace = True)
 print(households.info())
 
-plans = read_data('./data/input/plans777.csv', limit=1000)
+plans = read_data('./data/input/plans777.csv')
 print('plans import complete')
 print(plans.info())
 
