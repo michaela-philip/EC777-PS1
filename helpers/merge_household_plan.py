e97963ae8347669df769af1490e40f5df6ebb489
Michaela <michaela.philip@emory.edu>
2024-03-06 13:41:46 -0500

    merge 'outer'

--- helpers/merge_household_plan.py
+++ helpers/merge_household_plan.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def merge_data(filepath1, filepath2):
-    output = pd.merge(filepath1, filepath2, on='plan_name', how = 'left')
+    output = pd.merge(filepath1, filepath2, on='plan_name', how = 'outer')
     print("household merge complete")
     # print(output.head())
 
