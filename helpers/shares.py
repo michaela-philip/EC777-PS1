2d3c55e6ab5f57f35cd82b4dad8db39a97391276
Michaela <michaela.philip@emory.edu>
2024-03-06 12:17:18 -0500

    fix uninsured shares creation

--- helpers/shares.py
+++ helpers/shares.py
@@ -6,20 +6,25 @@ def get_shares(filepath):
     n_ind = filepath.groupby(['year', 'rating_area', 'plan_name'])['household_size'].sum()
     n_house = filepath.groupby(['year', 'rating_area', 'plan_name'])['household_size'].size()
 
-    market_level = pd.concat({'n_ind': n_ind, 'n_house' : n_house}, axis=1)
+    market_level = pd.concat({'n_ind': n_ind, 'n_house' : n_house}, axis=1).reset_index()
 
-    market_level['indiv_share'] = (market_level.groupby(['year', 'rating_area', 'plan_name'])['n_ind'].sum()) / (market_level.groupby(['year', 'rating_area'])['n_ind'].sum())
-    market_level['house_share'] = (market_level.groupby(['year', 'rating_area', 'plan_name'])['n_house'].size()) / (market_level.groupby(['year', 'rating_area'])['n_house'].size())
+    indiv_share = (market_level.groupby(['year', 'rating_area', 'plan_name'])['n_ind'].sum()) / (market_level.groupby(['year', 'rating_area'])['n_ind'].sum())
+    indiv_share = indiv_share.rename('indiv_share').reset_index()
+    house_share = (market_level.groupby(['year', 'rating_area', 'plan_name'])['n_house'].size()) / (market_level.groupby(['year', 'rating_area'])['n_house'].size())
+    house_share = house_share.rename('house_share').reset_index()
+
+    market_level = pd.merge(market_level, indiv_share, on=['year', 'rating_area', 'plan_name'])
+    market_level = pd.merge(market_level, house_share, on=['year', 'rating_area', 'plan_name'])
 
     market_level['ln_indiv_share'] = np.log(market_level['indiv_share'])
     market_level['ln_house_share'] = np.log(market_level['house_share'])
 
-    ln_uninsured_indiv = market_level.loc([market_level['plan_name'] == 'uninsured', 'ln_indiv_share'])
+    ln_uninsured_indiv = market_level.loc[market_level['plan_name'] == 'uninsured', 'ln_indiv_share']
     market_level['ln_uninsured_indiv'] = market_level.groupby(['year', 'rating_area'])['ln_indiv_share'].transform(lambda x: ln_uninsured_indiv.get(x.index))
-    market_level['ln_indiv_share_diff'] = market_level['ln_indiv_share'] - market_level['uninsured_ln_indiv_share']
+    market_level['ln_indiv_share_diff'] = market_level['ln_indiv_share'] - market_level['ln_uninsured_indiv']
 
-    ln_uninsured_house = market_level.loc([market_level['plan_name'] == 'uninsured', 'ln_house_share'])
+    ln_uninsured_house = market_level.loc[market_level['plan_name'] == 'uninsured', 'ln_house_share']
     market_level['ln_uninsured_house'] = market_level.groupby(['year', 'rating_area'])['ln_house_share'].transform(lambda x: ln_uninsured_house.get(x.index))
-    market_level['ln_house_share_diff'] = market_level['ln_house_share'] - market_level['uninsured_ln_house_share']    
+    market_level['ln_house_share_diff'] = market_level['ln_house_share'] - market_level['ln_uninsured_house']    
 
     return market_level
