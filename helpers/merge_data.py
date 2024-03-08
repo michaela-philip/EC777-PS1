d4e1defbd0c3bb1899d64faff1ec4effa0ee1db0
Michaela <michaela.philip@emory.edu>
2024-03-08 10:46:20 -0500

    restructure second merge

--- helpers/merge_data.py
+++ helpers/merge_data.py
@@ -7,8 +7,9 @@ def merge_data(filepath1, filepath2):
     print("household plan merge complete")
 
 def merge_market_household(household, market, plan):
-    market_minimal = market.drop(columns = ['indiv_share', 'house_share', 'ln_indiv_share', 'ln_house_share', 'ln_uninsured_indiv', 'ln_uninsured_house'])
-    intermed = pd.merge(household, market_minimal, on=['year', 'rating_area', 'plan_name'], how = 'outer')
-    output = pd.merge(intermed, plan, on = ['household_id', 'plan_name', 'year'], how = 'outer')
-
+    # market_minimal = market.drop(columns = ['indiv_share', 'house_share', 'ln_indiv_share', 'ln_house_share', 'ln_uninsured_indiv', 'ln_uninsured_house'])
+    # intermed = pd.merge(household, market_minimal, on=['year', 'rating_area', 'plan_name'], how = 'outer')
+    # output = pd.merge(intermed, plan, on = ['household_id', 'plan_name', 'year'], how = 'outer')
+    output = pd.merge(household, market, on=['year', 'rating_area', 'plan_name'], how = 'outer')
+    output = pd.merge(output, plan, on ='year', how = 'outer')
     return output
