515bc1b40500a6adfae0d33c5a59b4727755f3f9
Michaela <michaela.philip@emory.edu>
2024-03-11 13:14:26 -0400

    2a analysis

--- analysis/q2a.py
+++ analysis/q2a.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import sys
 import os
 import statsmodels.api as sm
@@ -12,6 +13,22 @@ from helpers.instruments import get_instruments
 market_data = pd.read_csv('./data/output/market_data.csv')
 market_data = get_instruments(market_data)
 
-endog = market_data['annual_price_pp']
-exog = market_data['perc_white', 'perc_male', 'fpl', 'year', 'rating_area']
+#Logit Model
+endog = market_data['avg_price_pp']
+exog = pd.get_dummies(market_data[['perc_white', 'perc_male', 'fpl', 'Insurer', 'AV', 'Metal_Level', 'HMO']], drop_first=True)
 dependent = market_data['ln_indiv_share_diff']
+instrument = market_data['instrument']
+
+logit_model = IV2SLS(dependent, exog, endog, instrument).fit()
+print(logit_model.summary)
+
+#Nested Logit Model
+nested_shares = (market_data.groupby(['year', 'rating_area', 'plan_name', 'Metal_Level'])['n_ind'].sum()) / (market_data.groupby(['year', 'rating_area', 'Metal_Level'])['n_ind'].sum())
+nested_shares = nested_shares.rename('nested_shares').reset_index()
+market_data = pd.merge(market_data, nested_shares, on=['year', 'rating_area', 'plan_name', 'Metal_Level'], how = 'outer')
+market_data['ln_nested_shares'] = np.log(market_data['nested_shares'])
+
+exog = pd.get_dummies(market_data[['perc_white', 'perc_male', 'fpl', 'Insurer', 'AV', 'Metal_Level', 'HMO', 'ln_nested_shares']], drop_first=True)
+
+nested_logit_model = IV2SLS(dependent, exog, endog, instrument).fit()
+print(nested_logit_model.summary)
--- helpers/instruments.py
+++ helpers/instruments.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 def get_instruments(filepath):
-    avg_prices = filepath.groupby(['year', 'plan_name', 'rating_area'])['annual_price_pp'].mean().reset_index()
+    avg_prices = filepath.groupby(['year', 'plan_name', 'rating_area'])['avg_price_pp'].mean().reset_index()
 
     def calculate_instrument(row):
         mask = (avg_prices['year'] == row['year']) & (avg_prices['plan_name'] == row['plan_name']) & (avg_prices['rating_area'] != row['rating_area'])
-        return avg_prices.loc[mask, 'annual_price_pp'].mean()
+        return avg_prices.loc[mask, 'avg_price_pp'].mean()
 
     filepath['instrument'] = filepath.apply(calculate_instrument, axis=1)
 
