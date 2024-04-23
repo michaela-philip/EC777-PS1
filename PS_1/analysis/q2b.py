edbd5df497d125fdb824a045eaa979bbcf88b7d4
Michaela <michaela.philip@emory.edu>
2024-04-23 15:46:26 -0400

    test numbers

--- PS_1/analysis/q2b.py
+++ PS_1/analysis/q2b.py
@@ -19,23 +19,26 @@ market_data = get_instruments(market_data)
 from analysis.q2a import nested_logit_AV, nested_logit_HMO
 
 #testing using fake numbers
-# J = 50
-# R = 500
-# K = 2   # Num random coefficients
-# Y = 15 #other observable characteristics
-# x = np.random.rand(J,(K+Y))
-# sigma = np.random.rand(K, K)
-# delta = np.random.rand(J)
-# z = np.random.rand(J,(K+Y))
-# W = np.eye((K+Y))
-# c = np.random.rand(J, K)
-# theta = np.random.rand(K)
+J = 50
+R = 500
+K = 2   # Num random coefficients
+Y = 15 #other observable characteristics
+x = np.random.rand(J,(K+Y))
+sigma = np.random.rand(K, K)
+delta = np.random.rand(J)
+z = np.random.rand(J,(K+Y))
+W = np.eye((K+Y))
+c = np.random.rand(J, K)
+theta = np.random.rand(K)
+nus = np.random.normal(0, 1, [R,K])
+obs = np.random.rand(J).reshape(-1,1)
+delta_0 = np.zeros(J).reshape(-1, 1)
 
 #test inner loop functions
 # mu = get_mu(c, theta, nus[0,:])
 # logit_share_test = predict_logit_share(delta, mu)
 # share_0 = predict_rc_logit_share(delta, c, theta, nus)
-# delta_1, share_1 = run_inner_loop(c, theta, nus, share_0, np.zeros(J))
+delta_1, share_1 = run_inner_loop(c, theta, nus, obs, delta_0)
 
 
 #test outer loop functions
@@ -47,15 +50,15 @@ from analysis.q2a import nested_logit_AV, nested_logit_HMO
 # #creating matrices
 # market_data = get_instruments(market_data)
 
-x = market_data[['Insurer', 'AV', 'Metal_Level', 'HMO', 'avg_price_hh', 'instrument']]
-z = market_data[['Insurer', 'AV', 'Metal_Level', 'HMO', 'avg_price_hh', 'instrument']]
-c = market_data[['Metal_Level', 'HMO']] 
-theta = np.array([nested_logit_AV, nested_logit_HMO])
-observed_share = market_data['ln_house_share']
-W = np.eye(x.shape[1])
-R = 500
-K = c.shape[1]
-c = c.values
+# x = market_data[['Insurer', 'AV', 'Metal_Level', 'HMO', 'avg_price_hh', 'instrument']]
+# z = market_data[['Insurer', 'AV', 'Metal_Level', 'HMO', 'avg_price_hh', 'instrument']]
+# c = market_data[['Metal_Level', 'HMO']] 
+# theta = np.array([nested_logit_AV, nested_logit_HMO])
+# observed_share = market_data['ln_house_share']
+# W = np.eye(x.shape[1])
+# R = 500
+# K = c.shape[1]
+# c = c.values
 
 np.random.seed(123)
 nus = np.random.normal(0, 1, [R,K])
