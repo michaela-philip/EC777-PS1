2c81025347c6e697957ce9c5a41ebf833ceb9ca5
Michaela <michaela.philip@emory.edu>
2024-04-23 15:46:37 -0400

    first commit

---
+++ PS_1/analysis/q2c.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+import statsmodels.api as sm
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(script_dir, '..'))
+
+from analysis.q2a import main
+
