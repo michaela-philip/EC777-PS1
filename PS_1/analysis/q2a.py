98698b2071736e548cd357de12a03177f32531ff
Michaela <michaela.philip@emory.edu>
2024-04-29 15:15:36 -0400

    remove blp code

--- PS_1/analysis/q2a.py
+++ PS_1/analysis/q2a.py
@@ -4,7 +4,7 @@ import sys
 import os
 import statsmodels.api as sm
 from linearmodels.iv import IV2SLS
-
+from stargazer.stargazer import Stargazer
 script_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(script_dir, '..'))
 os.chdir('PS_1')
@@ -16,6 +16,7 @@ market_data = get_instruments(market_data)
 market_data = market_data.dropna(subset=['AV', 'HMO'])
 market_data = market_data[market_data['house_share'] >= 0.01]
 
+
 #Logit Model
 endog = market_data['avg_price_pp']
 exog = pd.get_dummies(market_data[['perc_white', 'perc_male', 'fpl', 'Insurer', 'AV', 'Metal_Level', 'HMO']], drop_first=True)
@@ -24,6 +25,8 @@ instrument = market_data['instrument']
 
 logit_model = IV2SLS(dependent, exog, endog, instrument).fit()
 print(logit_model.summary)
+logit_stargazer = Stargazer([logit_model])
+
 
 #Nested Logit Model
 nested_shares = (market_data.groupby(['year', 'rating_area', 'plan_name', 'Metal_Level'])['n_ind'].sum()) / (market_data.groupby(['year', 'rating_area', 'Metal_Level'])['n_ind'].sum())
@@ -35,31 +38,10 @@ exog = pd.get_dummies(market_data[['perc_white', 'perc_male', 'fpl', 'Insurer', 
 
 nested_logit_model = IV2SLS(dependent, exog, endog, instrument).fit()
 print(nested_logit_model.summary)
+nested_logit_stargazer = Stargazer([nested_logit_model])
+
 
 #pull coefficients for later use
 nested_logit_AV = nested_logit_model.params['AV']
 nested_logit_HMO = nested_logit_model.params['HMO']
 print(nested_logit_AV, nested_logit_HMO)
-
-
-###BLP
-market_data = pd.read_csv('./data/output/market_data.csv')
-market_data = get_instruments(market_data)
-market_data = market_data.dropna(subset=['AV', 'HMO'])
-
-from helpers.inner_loop import run_inner_loop, predict_rc_logit_share, get_mu, predict_logit_share
-from helpers.outer_loop import outer_loop
-
-x = market_data[['Insurer', 'AV', 'Metal_Level', 'HMO', 'avg_price_hh', 'instrument']]
-z = market_data[['Insurer', 'AV', 'Metal_Level', 'HMO', 'avg_price_hh', 'instrument']]
-c = market_data[['AV', 'HMO']] 
-theta = np.array([nested_logit_AV, nested_logit_HMO])
-observed_share = np.array(market_data['house_share'].values).reshape(-1, 1)
-delta_0 = np.array(market_data['ln_house_share_diff']).reshape(-1, 1)
-W = np.eye(x.shape[1])
-R = 500
-K = c.shape[1]
-
-R = 500
-J = 50
-K = 2
