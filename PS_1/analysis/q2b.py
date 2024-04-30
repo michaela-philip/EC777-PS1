a5a842fde71ebfd82b044bc3ae7bd8bd947c5681
Michaela <michaela.philip@emory.edu>
2024-04-30 09:53:08 -0400

    drop missing and small values

--- PS_1/analysis/q2b.py
+++ PS_1/analysis/q2b.py
@@ -8,17 +8,19 @@ import scipy.optimize as opt
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(script_dir, '..'))
-# os.chdir('PS_1')
+os.chdir('PS_1')
 
-from helpers.inner_loop import run_inner_loop, predict_rc_logit_share, get_mu, predict_logit_share, market_year_inner_loop
-from helpers.outer_loop import outer_loop, market_year_outer_loop
+from helpers.inner_loop import market_year_inner_loop, run_inner_loop
+from helpers.outer_loop import market_year_outer_loop
 from helpers.instruments import get_instruments
 
 market_data = pd.read_csv('./data/output/market_data.csv')
 market_data = get_instruments(market_data)
-from analysis.q2a import nested_logit_AV, nested_logit_HMO, logit_stargazer, nested_logit_stargazer
+market_data = market_data.dropna(subset=['AV', 'HMO'])
+market_data = market_data[market_data['house_share']>=0.01]
+from analysis.q2a import nested_logit_AV, nested_logit_HMO
 
-np.random.seed(123)
+# np.random.seed(123)
 R = 500
 K = 2
 nus = np.random.normal(0, 1, [R,K])
@@ -27,3 +29,5 @@ theta = np.array([nested_logit_AV, nested_logit_HMO])
 #run BLP
 theta_2, beta = market_year_outer_loop(market_data, theta, nus, 500, 2)
 print(theta_2, beta)
+theta_2.to_pickle('data/output/"theta_2.pkl')
+beta.to_pickle('data/output/beta.pkl')
