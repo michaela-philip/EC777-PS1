978926bb89b7bbad54ee0424c5875188c370511e
Michaela <michaela.philip@emory.edu>
2024-04-30 09:55:49 -0400

    cd

--- PS_1/analysis/q2b.py
+++ PS_1/analysis/q2b.py
@@ -8,7 +8,7 @@ import scipy.optimize as opt
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(script_dir, '..'))
-os.chdir('PS_1')
+# os.chdir('PS_1')
 
 from helpers.inner_loop import market_year_inner_loop, run_inner_loop
 from helpers.outer_loop import market_year_outer_loop
