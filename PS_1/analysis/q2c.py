21fcee25354226a01de482818d6982067bc4eb14
Michaela <michaela.philip@emory.edu>
2024-05-02 13:50:35 -0400

    logit with age interaction and market data

--- PS_1/analysis/q2c.py
+++ PS_1/analysis/q2c.py
@@ -6,19 +6,9 @@ import statsmodels.formula.api as smf
 from stargazer.stargazer import Stargazer
 script_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(script_dir, '..'))
+# os.chdir('PS_1')
 
-main = pd.read_csv('./data/output/main.csv')
-main = main[main['household_size'] == 1]
+market_data = pd.read_csv('./data/output/market_data.csv')
 
-
-#Logit Model
-endog = market_data['avg_price_pp']
-exog = pd.get_dummies(market_data[['perc_white', 'perc_male', 'fpl', 'Insurer', 'AV', 'Metal_Level', 'HMO']], drop_first=True)
-dependent = market_data['ln_indiv_share_diff']
-instrument = market_data['instrument']
-
-logit_model = IV2SLS(dependent, exog, endog, instrument).fit()
-# print(logit_model.summary)
-# logit_stargazer = Stargazer([logit_model])
-
-logit_age = smf.ols(formula = 'ln_indiv_share_diff ~ avg_price_pp + perc_white + perc_male + fpl + Insurer + AV + Metal_Level + HMO + ')
+logit_age = smf.ols(formula = 'ln_indiv_share_diff ~ avg_price_pp + perc_white + perc_male + fpl + Insurer + AV + Metal_Level + HMO + perc_0to17 + perc_18to25 + perc_26to34 + perc_35to44 + perc_45to54 + perc_55to64 + perc_65plus + avg_price_pp*perc_0to17 + avg_price_pp*perc_18to25 + avg_price_pp*perc_26to34 + avg_price_pp*perc_35to44 + avg_price_pp*perc_45to54 + avg_price_pp*perc_55to64 + avg_price_pp*perc_65plus', data = market_data).fit()
+logit_age_stargazer = Stargazer([logit_age])
