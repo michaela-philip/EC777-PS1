0e6bde8f435b3124c98c50f339a31b9faa8f5a36
Michaela <michaela.philip@emory.edu>
2024-05-06 16:57:00 -0400

    clean up

--- PS_1/analysis/q2c.py
+++ PS_1/analysis/q2c.py
@@ -11,4 +11,3 @@ sys.path.append(os.path.join(script_dir, '..'))
 market_data = pd.read_csv('./data/output/market_data.csv')
 
 logit_age = smf.ols(formula = 'ln_indiv_share_diff ~ avg_price_pp + perc_white + perc_male + fpl + Insurer + AV + Metal_Level + HMO + perc_0to17 + perc_18to25 + perc_26to34 + perc_35to44 + perc_45to54 + perc_55to64 + perc_65plus + avg_price_pp*perc_0to17 + avg_price_pp*perc_18to25 + avg_price_pp*perc_26to34 + avg_price_pp*perc_35to44 + avg_price_pp*perc_45to54 + avg_price_pp*perc_55to64 + avg_price_pp*perc_65plus', data = market_data).fit()
-logit_age_stargazer = Stargazer([logit_age])
