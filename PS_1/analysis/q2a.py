d38c4cdc1e430f1e255fda529ec8f5470d177065
Michaela <michaela.philip@emory.edu>
2024-05-06 16:34:58 -0400

    display results as dataframe

--- PS_1/analysis/q2a.py
+++ PS_1/analysis/q2a.py
@@ -16,7 +16,6 @@ market_data = get_instruments(market_data)
 market_data = market_data.dropna(subset=['AV', 'HMO'])
 market_data = market_data[market_data['house_share'] >= 0.01]
 
-
 #Logit Model
 endog = market_data['avg_price_pp']
 exog = pd.get_dummies(market_data[['perc_white', 'perc_male', 'fpl', 'Insurer', 'AV', 'Metal_Level', 'HMO']], drop_first=True)
@@ -24,26 +23,22 @@ dependent = market_data['ln_indiv_share_diff']
 instrument = market_data['instrument']
 
 logit_model = IV2SLS(dependent, exog, endog, instrument).fit()
-# print(logit_model.summary)
-# logit_stargazer = Stargazer([logit_model])
-
 
 #Nested Logit Model
 nested_shares = (market_data.groupby(['year', 'rating_area', 'plan_name', 'Metal_Level'])['n_ind'].sum()) / (market_data.groupby(['year', 'rating_area', 'Metal_Level'])['n_ind'].sum())
 nested_shares = nested_shares.rename('nested_shares').reset_index()
 market_data = pd.merge(market_data, nested_shares, on=['year', 'rating_area', 'plan_name', 'Metal_Level'], how = 'outer')
 market_data['ln_nested_shares'] = np.log(market_data['nested_shares'])
-
 exog = pd.get_dummies(market_data[['perc_white', 'perc_male', 'fpl', 'Insurer', 'AV', 'Metal_Level', 'HMO', 'ln_nested_shares']], drop_first=True)
 
 nested_logit_model = IV2SLS(dependent, exog, endog, instrument).fit()
-# print(nested_logit_model.summary)
-# nested_logit_stargazer = Stargazer([nested_logit_model])
-
 
 #pull coefficients for later use
 nested_logit_AV = nested_logit_model.params['AV']
 nested_logit_HMO = nested_logit_model.params['HMO']
-# print(nested_logit_AV, nested_logit_HMO)
 
-berry_inversion_stargazer = Stargazer([logit_model, nested_logit_model])
+#create results dataframe
+logit_df = pd.DataFrame(logit_model.params)
+nested_logit_df = pd.DataFrame(nested_logit_model.params)
+results_2a = pd.concat([logit_df, nested_logit_df], axis = 1)
+results_2a.columns = ['Logit', 'Nested Logit']
