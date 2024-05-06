6dcc5f12a42a0582bd6029c0324e4df7e8ff59d9
Michaela <michaela.philip@emory.edu>
2024-05-06 14:38:22 -0400

    typo

--- PS_1/data_code/clean_counterfactual_data.py
+++ PS_1/data_code/clean_counterfactual_data.py
@@ -20,7 +20,7 @@ main_cf = merge_household_price(households_plan, household_plan_year)
 #don't include penalty for counterfactual
 main_cf['annual_price'] = (main_cf['premium'] - main_cf['subsidy']) * 12
 main_cf['annual_price_pp'] = main_cf['annual_price'] / main_cf['household_size']
-market_data_cf_cf = merge_market_price(main_cf, market_data_cf)
+market_data_cf = merge_market_price(main_cf, market_data_cf)
 
 main_cf = main_cf.dropna(subset=['rating_area'])
 main_cf = main_cf.drop(main_cf[main_cf['plan_name'] == 'Uninsured'].index)
